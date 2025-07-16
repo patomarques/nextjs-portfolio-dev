40b300cd749366cfb1de0b4dccaf054133d17152
Pato Marques <patoxmarques@gmail.com>
2025-07-16 02:22:48 -0300

    starter

--- components/shared/AppFooter.jsx
+++ components/shared/AppFooter.jsx
@@ -8,31 +8,31 @@ import {
 import AppFooterCopyright from './AppFooterCopyright';
 
 const socialLinks = [
-	{
-		id: 1,
-		icon: <FiGlobe />,
-		url: 'https://www.stoman.me/',
-	},
+	// {
+	// 	id: 1,
+	// 	icon: <FiGlobe />,
+	// 	url: 'https://www.stoman.me/',
+	// },
 	{
 		id: 2,
 		icon: <FiGithub />,
-		url: 'https://github.com/realstoman',
+		url: 'https://github.com/patomarques',
 	},
-	{
-		id: 3,
-		icon: <FiTwitter />,
-		url: 'https://twitter.com/realstoman',
-	},
+	// {
+	// 	id: 3,
+	// 	icon: <FiTwitter />,
+	// 	url: 'https://twitter.com/realstoman',
+	// },
 	{
 		id: 4,
 		icon: <FiLinkedin />,
-		url: 'https://www.linkedin.com/in/realstoman',
+		url: 'https://www.linkedin.com/in/dayvsonmarques',
 	},
-	{
-		id: 5,
-		icon: <FiYoutube />,
-		url: 'https://www.youtube.com/c/realstoman',
-	},
+	// {
+	// 	id: 5,
+	// 	icon: <FiYoutube />,
+	// 	url: 'https://www.youtube.com/c/realstoman',
+	// },
 ];
 
 function AppFooter() {
@@ -42,7 +42,7 @@ function AppFooter() {
 				{/* Footer social links */}
 				<div className="font-general-regular flex flex-col justify-center items-center mb-12 sm:mb-28">
 					<p className="text-3xl sm:text-4xl text-primary-dark dark:text-primary-light mb-5">
-						Follow me
+						Siga-me
 					</p>
 					<ul className="flex gap-4 sm:gap-8">
 						{socialLinks.map((link) => (
---
+++ pages/education.jsx
@@ -0,0 +1,9 @@
+function education() {
+	return (
+		<div>
+			<h1 className="title">Educação</h1>
+		</div>
+	);
+}
+
+export default education;
